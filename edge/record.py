6f904bafe824e9c37beff86e085a6dd525a37143
OZH2005 <owenzhall@gmail.com>
2025-11-09 00:18:49 -0600

    Happy HACKING!

--- edge/record.py
+++ edge/record.py
@@ -1,7 +1,7 @@
 import pyaudio
 import wave
 from pynput import keyboard
-from ASR import transcribe
+import threading
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -13,51 +13,39 @@ WAVE_OUTPUT_FILENAME = "output.wav"
 record = True
 p = pyaudio.PyAudio()
 
-def on_press(key):
-    if key.char == ('d'):
-        global record 
-        record = not record
-
-def on_release(key):
-    pass
-
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
-print("Recording")
-
 frames = []
+record = False
 
-for i in range(100000):
-    if record:
+async def start_recording(INPT):
+    global frames
+    frames = []
+    global record
+    record = INPT
+    threading.Thread(target=update).start()
+
+def update():
+    while record: 
         data = stream.read(CHUNK)
         frames.append(data)
-    else:
-        exit
 
-listener.stop()
-listener.join()
-print("Stopped Recording")
+def stop_recording():
+    global record
+    record = False
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    get_wav()
 
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-try:
-    print("Transcription:")
-    print(transcribe())
-except:
-    print("transcription failed")
+def get_wav():
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
